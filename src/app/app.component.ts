76296918e382cfed9e44d9220dbac10314d72803
Said <46088969+findsaid@users.noreply.github.com>
2022-06-04 21:20:24 +0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-angular-app';
+  title = 'todo-angular-app'
+  todos: any = []
+  addTodo(todo: string) {
+    if (todo !== "") {
+      this.todos.push(todo)
+    } else {
+      alert('Field is empty!')
+    }
+  }
+  deleteTodo(todo: string) {
+    for (let i = 0; i <= this.todos.length; i++) {
+      if (todo == this.todos[i]) {
+        this.todos.splice(i, 1)
+      }
+    }
+  }
 }
